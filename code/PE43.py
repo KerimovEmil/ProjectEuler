aa69326342bb39fadd649c213175a5792eb8e1e2
KerimovEmil <emilkerimov93@gmail.com>
2018-07-13 18:36:35 -0400

    Added py43. Needs a lot of clean up. Not super fast.

---
+++ code/PE43.py
@@ -0,0 +1,45 @@
+# The number, 1406357289, is a 0 to 9 pandigital number because it is made up of each of the digits 0 to 9 in some order, but it also has a rather interesting sub-string divisibility property.
+
+# Let d1 be the 1st digit, d2 be the 2nd digit, and so on. In this way, we note the following:
+
+# d2d3d4=406 is divisible by 2
+# d3d4d5=063 is divisible by 3
+# d4d5d6=635 is divisible by 5
+# d5d6d7=357 is divisible by 7
+# d6d7d8=572 is divisible by 11
+# d7d8d9=728 is divisible by 13
+# d8d9d10=289 is divisible by 17
+# Find the sum of all 0 to 9 pandigital numbers with this property.
+
+
+# Answer = 16695334890
+
+import itertools
+
+sum = 0
+L = itertools.permutations(range(10))
+
+
+def check(num):
+    if int(str(num)[1:4]) % 2:
+        return False
+    if int(str(num)[2:5]) % 3:
+        return False
+    if int(str(num)[3:6]) % 5:
+        return False
+    if int(str(num)[4:7]) % 7:
+        return False
+    if int(str(num)[5:8]) % 11:
+        return False
+    if int(str(num)[6:9]) % 13:
+        return False
+    if int(str(num)[7:10]) % 17:
+        return False
+    return True
+
+
+for i in L:
+    num = int(''.join(map(str, list(i))))
+    if check(num):
+        sum += num
+print(sum)
