b1b79e56e032f6fdad8d6eadb6be897aa056f2d1
KerimovEmil <emilkerimov93@gmail.com>
2018-08-19 23:28:19 -0400

    simplified the functions in pe38 greatly

--- code/PE38.py
+++ code/PE38.py
@@ -14,25 +14,21 @@
 # Answer = 932718654
 
 
-def unique_digits(num, existing_digits):
+def unique_digits(str_num):
     """
-    Test that the digits of the input number are unique and are not in the set of existing digits.
+    Test that the digits of the input number are unique and are not 0.
     Args:
-        num: <str> integer to test
-        existing_digits: <set> digits not allowed (optional)
+        str_num: <str> integer to test
 
-    Returns: False if the digits of the input number are not unique or are in the set of existing digits,
-        else returns new set of used digits.
+    Returns: False if the digits of the input number are not unique else True
     """
-    str_num_set = set(str(num))
+    str_num_set = set(str_num)
     if '0' in str_num_set:
         return False
-    if len(str_num_set) != len(str(num)):
-        return False
-    if len(str_num_set.intersection(existing_digits)) > 0:
+    elif len(str_num_set) != len(str_num):
         return False
     else:
-        return str_num_set.union(existing_digits)
+        return True
 
 
 class Problem38:
@@ -41,25 +37,20 @@ class Problem38:
         self.n_digit = n_digit
         self.max = 0
 
-    def create_sample_pandigital(self, a):
-        # ensure that the initial number has no repeated digits
-        x = unique_digits(a, set())
-        if x is False:
+    def create_sample_pandigital(self, num):
+        num_str = str(num)
+        # ensure that the initial number has no repeated digits or 0
+        if unique_digits(num_str) is False:
             return 0
         # multiply the number up to the digit 9
         for i in range(2, self.n_digit):
+            num_str += str(num * i)
             # check if still no repeated digits
-            x = unique_digits(a * i, x)
-            if x is False:
+            if unique_digits(num_str) is False:
                 break
             # if still no repeated digits and length is 9 then return solution
-            if len(x) == self.n_digit:
-                r = str(a)
-                i = 2
-                while len(r) < self.n_digit:
-                    r += str(a * i)
-                    i += 1
-                return int(r)
+            if len(num_str) == self.n_digit:
+                return int(num_str)
         return 0
 
     def solve(self):
