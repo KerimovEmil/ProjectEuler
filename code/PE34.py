6b2bb6f1281b622e2c7d84edbf98f799c5df68aa
KerimovEmil <emilkerimov93@gmail.com>
2018-08-21 00:19:38 -0400

    sped up pe34

--- code/PE34.py
+++ code/PE34.py
@@ -5,7 +5,9 @@
 # ANSWER:
 # 40730
 
-from util.utils import basic_factorial
+# from util.utils import basic_factorial
+
+factorials_0_to_9 = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880]
 
 
 class Problem34:
@@ -16,7 +18,8 @@ class Problem34:
     @staticmethod
     def sum_of_factorial_of_digits(number):
         """Sum of the factorial of digits"""
-        return sum([basic_factorial(int(i)) for i in str(number)])
+        # return sum([basic_factorial(int(i)) for i in str(number)])
+        return sum([factorials_0_to_9[int(i)] for i in str(number)])
 
     def solve(self):
         for i in range(3, self.max_x):
@@ -26,12 +29,11 @@ class Problem34:
 
 
 if __name__ == "__main__":
-    obj = Problem34(max_x=200000)
+    obj = Problem34(max_x=9999999)
     sol = obj.solve()
     print(sol)
     # since 9! = 362880
     # 9999999 is an easy upper limit to come up with. 7 times 9! is less than 9999999.
-    # todo: add math proof why 200,000 is the maximum.
     # https://en.wikipedia.org/wiki/Factorion
     # only four such numbers exist: 1,2, 145, 40585.
     # since 1 and 2 are not sums as the question stated then the answer is 145+40585 = 40730
