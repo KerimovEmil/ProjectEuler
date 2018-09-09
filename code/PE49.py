147acc4368bf056d8bec2f706c861c60f3bfd04c
KerimovEmil <emilkerimov93@gmail.com>
2018-09-08 23:19:36 -0400

    Brought pe49 up to code standards

--- code/PE49.py
+++ code/PE49.py
@@ -1,29 +1,56 @@
-# The arithmetic sequence, 1487, 4817, 8147, in which each of the terms increases by 3330, is unusual in two ways: (i) each of the three terms are prime, and, (ii) each of the 4-digit numbers are permutations of one another.
+# The arithmetic sequence, 1487, 4817, 8147, in which each of the terms increases by 3330, is unusual in two ways:
+# (i) each of the three terms are prime, and,
+# (ii) each of the 4-digit numbers are permutations of one another.
 
-# There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes, exhibiting this property, but there is one other 4-digit increasing sequence.
+# There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes, exhibiting this property,
+# but there is one other 4-digit increasing sequence.
 
 # What 12-digit number do you form by concatenating the three terms in this sequence?
 
-# Answer: 2969, 6299, 9629
+# Answer: 2969, 6299, 9629 : 296962999629
 
 from util.utils import sieve
+from util.utils import timeit
 
 
-P = list(sieve(10000))
-P2 = list(sieve(1000))
-T = P[len(P2):]
-Q = set(T)
-U = []
-for x in T:
-    for i in range(1, 5000):
-        if ((x + i) in Q) and ((x + 2 * i) in Q):
-            if set(str(x)) == set(str(x + i)):
-                if set(str(x)) == set(str(x + 2 * i)):
-                    U.append(x)
-                    U.append(x + i)
-                    U.append(x + 2 * i)
+class Problem49:
+    def __init__(self, exclude, n):
+        self.exclude = exclude
+        self.n = n
+        self.ans = None
 
-print(U)
-ls_sol = [str(x) for x in U if x not in {1487, 4817, 8147}]
+    @timeit
+    def solve(self):
+        max_prime = int(10**self.n)
+        min_prime = int(10**(self.n-1))
 
-print(''.join(ls_sol))
+        P = list(sieve(max_prime))  # all primes less than 10000
+        P2 = list(sieve(min_prime))
+        T = P[len(P2):]  # remove primes less than 1000
+        Q = set(T)
+        U = []
+        for x in T:
+            for i in range(1, int(max_prime/2)):
+                if ((x + i) in Q) and ((x + 2 * i) in Q):
+                    if set(str(x)) == set(str(x + i)):
+                        if set(str(x)) == set(str(x + 2 * i)):
+                            U.append(x)
+                            U.append(x + i)
+                            U.append(x + 2 * i)
+
+        ls_sol = [str(x) for x in U if x not in self.exclude]
+
+        self.ans = ''.join(ls_sol)
+
+        return self.ans
+
+    def get_solution(self):
+        return self.ans
+
+
+if __name__ == "__main__":
+    exclude = {1487, 4817, 8147}
+    n = 4
+    obj = Problem49(exclude=exclude, n=n)
+    sol = obj.solve()
+    print(sol)
