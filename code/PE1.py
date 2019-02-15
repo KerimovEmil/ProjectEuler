ebc0fb0cae480a9b270624ba61773c16378b7f06
KerimovEmil <emilkerimov93@gmail.com>
2019-02-14 23:51:42 -0500

    added doc string to pe1

--- code/PE1.py
+++ code/PE1.py
@@ -1,3 +1,16 @@
+"""
+PROBLEM
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+ANSWER:
+233168
+Solve time ~ 0.002 seconds
+"""
+
 from util.utils import timeit
 import unittest
 
