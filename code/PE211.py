02890df45398e81e096ab660b30fc71c086e13a6
KerimovEmil <emilkerimov93@gmail.com>
2019-02-14 23:37:30 -0500

    combined the new util function into a previous one.

--- code/PE211.py
+++ code/PE211.py
@@ -4,6 +4,7 @@
 # Find the sum of all n, 0 < n < 64,000,000 such that σ2(n) is a perfect square.
 
 # Answer = 1922364685
+# solve time ~ way too long.
 
 # n = p1^x p2^y p3^z
 # σ2(n) = (1 + p1^2 + p1^4 + ... + p1^2x) * (1 + p2^2 + p2^4 + ... + p2^2y) * (1 + p3^2 + p3^4 + ... + p3^2z)
@@ -17,9 +18,10 @@
 
 import unittest
 from util.utils import timeit
-from util.utils import primes_of_n_with_ls_prime
+from util.utils import primes_of_n
 from util.utils import sieve
 # import math
+# from functools import lru_cache
 
 
 class Problem211:
@@ -43,7 +45,7 @@ class Problem211:
         #         self.dc_prime_factor_sq_sum[(p, i)] = primes_of_n(self.dc_sq_sum[(p, i)])
 
         for i in range(self.max_int):
-            dc_factors = primes_of_n_with_ls_prime(i, ls_primes)
+            dc_factors = primes_of_n(i, ls_primes)
             sum_sqs = self.sum_sq_divisors(dc_factors)
 
             if self.test_square(sum_sqs):
@@ -56,6 +58,7 @@ class Problem211:
         sqrtx = x ** 0.5
         return abs(sqrtx - int(sqrtx)) < 1e-14
 
+    # @lru_cache(maxsize=None)
     def sum_sq_divisors_prime(self, prime, factor):
         if (prime, factor) in self.dc_sq_sum:
             return self.dc_sq_sum[(prime, factor)]
@@ -76,9 +79,11 @@ class Problem211:
 class Solution221(unittest.TestCase):
     def setUp(self):
         self.problem = Problem211(max_int=64000000)
+        # self.problem = Problem211(max_int=100)
 
     def test_solution(self):
         self.assertEqual(1922364685, self.problem.solve())
+        # self.assertEqual(43, self.problem.solve())
 
 
 if __name__ == '__main__':
