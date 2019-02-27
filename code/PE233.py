c27f41c73f17b22f3cb6bec664484798aa43b1c0
KerimovEmil <emilkerimov93@gmail.com>
2019-02-27 00:11:21 -0500

    pe233, changing the problem description to account for odd N. Removing the condition that N must be even. Changing the head of sub options to not be square. Starting with a fac_2 of 1 instead of 0.

--- code/PE233.py
+++ code/PE233.py
@@ -31,35 +31,33 @@ import unittest
 # important point, all primes which are 1 mod 4 factor exactly into 2 Gaussian primes, all primes which are 3 mod 4
 # do not factor into Gaussian primes. Note that 2 does factor into (1+i) and (1-i) but those are not unique up to a
 # multiple of i, so they won't really matter for the counting of G(N). This is why G(N) = f(N) (for even N) since
-# f(N) = length of the set such that {a^2 + b^2 = N^2/2 for a,b integers}
+# f(N) = length of the set such that {a^2 + b^2 = 2*N^2 for a,b integers}. The reason it is 2*N^2 is due to the
+# inscribed angle in a half circle is always 90 degrees.
 
 # We can use this to quickly count f(N).
 # After we factor N, we first ignore all of the primes which are 3 mod 4 (and 2). The remainder are primes which are a
 # multiplication of 2 Gaussian primes. After we square the number we double the exponents on these prime factors.
-# Remember we are looking for two conjugate Gaussian integers which multiply to give us N^2. It is equivalent to
+# Remember we are looking for two conjugate Gaussian integers which multiply to give us 2*N^2. It is equivalent to
 # finding the number of factors of N^2.
 
 # Example: N = 10,000 = 2^4 * 5^4.
-# N^2/2 = 2^7 * 5^8
+# 2*N^2 = 2^9 * 5^8
 # only looking at the primes 1 mod 4, we have 5^8.
 # there are (1+8) choices of factors, and we multiply by 4 for the 4 possible rotations (1,-1,i,-i)
 # hence f(10,000) = 9*4 = 36
 
 # therefore N such that f(N) = 420 implies that
-# 1) N must be even. Otherwise N^2/2 is not an integer.  # todo, on second thought, I don't think this is true.
-# 2) N^2 must have exactly 3 primes of 1 mod 4, with exponents 2,4,6. (420/4 = 105 = 3*5*7)
+# 1) N^2 must have exactly 3 primes of 1 mod 4, with exponents 2,4,6. (420/4 = 105 = 3*5*7)
 # This implies that N must exactly 3 primes of 1 mod 4, with exponents 1,2,3.
 # Note if only 2 prime factors made up the 105 multiples then N would be bigger than 10^11, since
 # 5**10 * 13**10 = 1346274334462890625
 
-# Since there is an infinite amount of ways to construct this, we must use the 3rd condition in the problem
-# 3) N ≤ 10^11
+# Since there is an infinite amount of ways to construct this, we must use the 2nd condition in the problem
+# 2) N ≤ 10^11
 
 # Note the first few primes with 1 mod 4 are 5, 13, 17, 29, 37, 41, 53, ...
-# therefore the smallest possible N would be 2 * 5^3 * 13^2 * 17^1 = 718250
-# The largest possible prime 1 mod 4 to consider would be 2366809 since 2 * 5^3 * 13^2 * 2366809^1 = 99997680250
+# therefore the smallest possible N would be 5^3 * 13^2 * 17^1 = 718250
 
-# There are only 86929 primes less then or equal to 2366809 which are 1 mod 4.
 # For each combination we need to also count multiples of every other prime number that keep N below 10^11.
 
 # The largest prime that can be cubed is 521, since 5^2 * 13^1 * 677^3 = 100843838225
@@ -75,7 +73,8 @@ class Problem233:
     def calculate_options(chosen_n, sub_options, sofar):
         if not len(sub_options):
             return sofar
-        head = sub_options[0] ** 2
+        # head = sub_options[0] ** 2
+        head = sub_options[0]
         new_chosen_n = head * chosen_n
         if (new_chosen_n <= 1e11):
             sofar.add(new_chosen_n)
@@ -87,12 +86,14 @@ class Problem233:
     @staticmethod
     def get_variants(p1, p2, p3, others, options):
         n_max = 1e11
-        chosen_n = 2 * (p1 ** 1) * (p2 ** 2) * (p3 ** 3)
+        # chosen_n = 2 * (p1 ** 1) * (p2 ** 2) * (p3 ** 3)
+        chosen_n = (p1 ** 1) * (p2 ** 2) * (p3 ** 3)
         # max multiplicative limit
         limit = n_max / chosen_n
         sub_others = [x for x in others if x <= limit]
 
-        fac_2 = 0
+        # fac_2 = 0
+        fac_2 = 1
         while True:
             cn = (2 ** fac_2) * chosen_n
             if cn > n_max:
@@ -113,7 +114,8 @@ class Problem233:
                     continue
 
                 for p1 in p1s:
-                    if (2 * p3 ** 3 * p2 ** 2 * p1 ** 1) > 1e11:
+                    # if (2 * p3 ** 3 * p2 ** 2 * p1 ** 1) > 1e11:
+                    if (p3 ** 3 * p2 ** 2 * p1 ** 1) > 1e11:
                         break
                     if (p1 == p2):
                         continue
