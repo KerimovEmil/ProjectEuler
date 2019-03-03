d3ccb8b4b1e2de5e472043dedf2d9df26d7fc32b
KerimovEmil <emilkerimov93@gmail.com>
2019-03-03 14:00:44 -0500

    Commiting WIP code that doesn't yet work, but better writeup

--- code/pe229.py
+++ code/pe229.py
@@ -45,7 +45,8 @@ import unittest
 # D = −11, -7, -3, -2, −1, 2, 3, 5, 6, 7, 11, 13, 17, 19, 21, 29, 33, 37, 41, 57, 73
 
 # Using the Legendre symbol (-1/p) = {1 if p==1 mod 4, -1 if p==3 mod 4}
-# In other words -1 has quadratic residues if p == 1 mod 4
+# In other words -1 has quadratic residues iff p == 1 mod 4,
+# i.e. x^2 == -1 mod p has solutions for x iff p == 1 mod 4
 
 # we can use legendre symbol arithmetic, (a/p)*(b/p) = (ab/p) to prove a lot more of these quadratic residues.
 # We get:
@@ -55,23 +56,58 @@ import unittest
 # -7 has quadratic residues if p == 1,2,4 mod 7
 
 # Therefore if p == 1 mod 4 AND p == 1,3 mod 8 AND p == 1 mod 3 AND p == 1,2,4 mod 7
-# then
+# then x^2 == -1, -2, -3, -7  mod p has a solution.
+
+# Note through all of this we assumed p was an odd prime.
+
+# Example 3600: Divide out all of the powers of 2 and we get: 3600/16 = 225.
+# 225 mod 4 = 1, 225 mod 8 = 1, 225 mod 7 = 1
+# 225 mod 3 = 0 is a special case since 3 divides 225, and the legendre symbol arithmetic assumes they are coprime
+
+# Example 88201:
+# 88201 % 4 = 1, 88201 % 3 = 1, 88201 % 8 = 1, 88201 % 7 = 1
 
 class Problem229:
-    def __init__(self):
-        pass
+    def __init__(self, max_n):
+        self.max_n = max_n
+        self.count = 0
+
+    @staticmethod
+    def legendre_neg1(n):
+        if n % 4 == 1:
+            return 1
+        elif n % 4 == 3:
+            return -1
+        else:
+            return 1
+
 
     @timeit
     def solve(self):
-        return None
+        # for i in range(1, self.max_n + 1, 2):  # only looping odd numbers
+        for i in range(1, self.max_n + 1):
+            highest_multiple_of_2 = i & -i
+
+            k = i // highest_multiple_of_2
+
+            cond1 = k % 4 == 1  # not needed
+
+            cond2 = k % 8 == 1
+            # cond3 = (i % 3 == 1) or i % 3 == 0
+            cond3 = (k % 3 == 1)
+            # cond4 = (i % 7 == 1) or i % 7 == 0
+            cond4 = (k % 7 == 1)
+            if cond1 and cond2 and cond3 and cond4:
+                self.count += 1
+        return self.count
 
 
 class Solution229(unittest.TestCase):
     def setUp(self):
-        self.problem = Problem229()
+        self.problem_small = Problem229(max_n=int(1e7))
 
     def test_solution(self):
-        self.assertEqual(None, self.problem.solve())
+        self.assertEqual(75373, self.problem_small.solve())
 
 
 if __name__ == '__main__':
