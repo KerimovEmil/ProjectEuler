137acd241d4c012a08e39b137c6e8b37feb3d918
KerimovEmil <emilkerimov93@gmail.com>
2019-08-31 12:26:01 -0400

    PE92: formatting the file and stating the initial runtime of the solution

--- code/PE92.py
+++ code/PE92.py
@@ -1,16 +1,21 @@
-# PROBLEM
+"""
+PROBLEM
 
-# A number chain is created by continuously adding the square of the digits in a number to form a new
-# number until it has been seen before.
+A number chain is created by continuously adding the square of the digits in a number to form a new
+number until it has been seen before.
 
-# Therefore any chain that arrives at 1 or 89 will become stuck in an endless loop.
-# What is most amazing is that EVERY starting number will eventually arrive at 1 or 89.
-# How many starting numbers below ten million will arrive at 89?
+Therefore any chain that arrives at 1 or 89 will become stuck in an endless loop.
+What is most amazing is that EVERY starting number will eventually arrive at 1 or 89.
+How many starting numbers below ten million will arrive at 89?
 
-# ANSWER
-# 8581146
+ANSWER:
+8581146
+Solve time ~76 seconds
+"""
 
 import functools
+from util.utils import timeit
+import unittest
 
 
 def memoize(obj):  # todo: move to utils? or not even use his
@@ -39,13 +44,20 @@ class Problem92:
         self.n = n
         self.count = 0
 
+    @timeit
     def solve(self):
         for i in range(1, self.n):
             self.count += eightynine(i)
         return self.count
 
 
-if __name__ == "__main__":
-    obj = Problem92(int(1e7))
-    sol = obj.solve()
-    print(sol)
+class Solution92(unittest.TestCase):
+    def setUp(self):
+        self.problem = Problem92(int(1e7))
+
+    def test_solution(self):
+        self.assertEqual(8581146, self.problem.solve())
+
+
+if __name__ == '__main__':
+    unittest.main()
