19a2f7a8739d5963855a703a69b9df18353838df
KerimovEmil <emilkerimov93@gmail.com>
2019-08-31 13:33:52 -0400

    PE92: added cut-off logic to remove values stored in cache, and added math logic justifying it. Reduced time to 58 seconds.

--- code/PE92.py
+++ code/PE92.py
@@ -10,22 +10,31 @@ How many starting numbers below ten million will arrive at 89?
 
 ANSWER:
 8581146
-Solve time ~70 seconds
+Solve time ~58 seconds
 """
 
 import functools
 from util.utils import timeit
 import unittest
 
+# define s(n) to be the sum of the square of the digits of n
+# hence if n = sum_{i=0}^{r} d_i 10^i  then s(n) = sum_{i=0}^{r} d_i^2
+# note that if r >= 3 then n > s(n):
+# Proof:
+# since d_i < 10 for all i, for r >=3
+# d_0 + d_r * 10^r >= 1000 >= 162 = 9^2 + 9^2 >= d_0^2 + d_r^2
+# hence each new digit added still implies that n > s(n) if there are at least 3 digits (i.e. [1, 999]
+
 
 @functools.lru_cache(maxsize=None, typed=False)
-def eightynine(d):  # TODO: this is very slow, need to speed up
+def eighty_nine(d):  # TODO: this is very slow, need to speed up. The slowest part is taking all of the sum of squares
+    print(d)
     if d == 89:
         return True
     elif d == 1:
         return False
     else:
-        return eightynine(sum(int(c) ** 2 for c in str(d)))
+        return eighty_nine(sum(int(c) ** 2 for c in str(d)))
 
 
 class Problem92:
@@ -33,10 +42,24 @@ class Problem92:
         self.n = n
         self.count = 0
 
+    @staticmethod
+    def _eighty_nine_no_cache(d):
+        return eighty_nine(sum(int(c) ** 2 for c in str(d)))
+
     @timeit
     def solve(self):
-        for i in range(1, self.n):
-            self.count += eightynine(i)
+        cut_off = 1000
+        # cut_off = self.n
+        self.count += sum([eighty_nine(i) for i in range(1, cut_off)])
+        # for i in range(1, cut_off):
+        #     # print("----------")
+        #     self.count += eighty_nine(i)
+
+        self.count += sum([self._eighty_nine_no_cache(i) for i in range(cut_off, self.n)])
+        # for i in range(cut_off, self.n):
+        #     # print("----------")
+        #     self.count += self._eighty_nine_no_cache(i)
+
         return self.count
 
 
