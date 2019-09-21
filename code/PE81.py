63795165a0ea54d7c7c5b887f0446da3a62ec9d1
KerimovEmil <emilkerimov93@gmail.com>
2019-09-21 19:34:08 -0400

    Cleaning up solution 81

--- code/PE81.py
+++ code/PE81.py
@@ -1,28 +1,32 @@
-# PROBLEM
+"""
+PROBLEM
 
-# Find the minimal path sum, in matrix.txt (right click and "Save Link/Target As..."),
-# a 31K text file containing a 80 by 80 matrix, from the top left to the bottom right by only moving right and down.
+Find the minimal path sum, in matrix.txt (right click and "Save Link/Target As..."),
+a 31K text file containing a 80 by 80 matrix, from the top left to the bottom right by only moving right and down.
 
-# ANSWER
-# 427337
+ANSWER:
+427337
+Solve time ~0.012 seconds
+"""
 
 import copy
+from util.utils import timeit
+import unittest
 
 
 class Problem81:
     def __init__(self, data_path):
-        self.problem_data = []
-        self.load_data(data_path)
+        self.problem_data = self.load_data(data_path)
         self.n = len(self.problem_data)
         self.working_matrix = copy.deepcopy(self.problem_data)
 
-    def load_data(self, data_path):
-        f = open(data_path, 'r')
-        for line in f:
-            w = line.split(',')
-            w = [int(x) for x in w]
-            self.problem_data.append(w)
+    @staticmethod
+    def load_data(data_path):
+        with open(data_path, 'r') as f:
+            problem_data = [[int(n) for n in s.split(',')] for s in f.readlines()]
+        return problem_data
 
+    @timeit
     def solve(self):
         B = self.working_matrix
         n = self.n
@@ -40,7 +44,13 @@ class Problem81:
         return B[0][0]
 
 
-if __name__ == "__main__":
-    obj = Problem81(r'..\problem_data\p081_matrix.txt')
-    sol = obj.solve()
-    print(sol)
+class Solution81(unittest.TestCase):
+    def setUp(self):
+        self.problem = Problem81(r'..\problem_data\p081_matrix.txt')
+
+    def test_solution(self):
+        self.assertEqual(427337, self.problem.solve())
+
+
+if __name__ == '__main__':
+    unittest.main()
