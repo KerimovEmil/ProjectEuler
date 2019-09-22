49de09adf43df2630d91bf8c979b81d996171e80
KerimovEmil <emilkerimov93@gmail.com>
2019-09-21 22:14:27 -0400

    Simplified 37 and sped it up

--- code/PE37.py
+++ code/PE37.py
@@ -10,7 +10,7 @@ NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
 
 ANSWER:
 748317
-Solve time ~7.3 seconds
+Solve time ~0.3 seconds
 """
 
 from util.utils import timeit, sieve
@@ -25,53 +25,35 @@ def trunc_right(num):
     return int(str(num)[1:])
 
 
-def check1(num):
-    """Checks if no digit of num is divisible by 2."""
-    for i in str(num):
-        if int(i) % 2 == 0:
-            return False
-    return True
-
-
 class Problem37:
     def __init__(self, max_int):
         self.max_int = max_int
         self.ans = 0
-        self.count = 0
-
-    def create_list_of_primes(self):
-        ls_primes = list(sieve(self.max_int))
-        ls_primes_loop = []
-        for prime in ls_primes:
-            if check1(prime):  # no digit will be divisible by 2
-                ls_primes_loop.append(prime)
-            elif len(str(prime)) == 2:  # all two digit primes are included even if they have a 2
-                ls_primes_loop.append(prime)
-        return ls_primes_loop, ls_primes
 
     @timeit
     def solve(self):
-        ls_primes_loop, ls_primes = self.create_list_of_primes()
-        for prime in ls_primes_loop:
+        set_primes = set(sieve(self.max_int))
+        for prime in set_primes:
+            if prime in {2, 3, 5, 7}:  # problem statement
+                continue
+
             trunc_l = prime
             trunc_r = prime
-            k = len(str(prime))
 
-            if k > 1:
-                count = 0
-                for i in range(k - 1):
-                    trunc_l = trunc_left(trunc_l)
-                    trunc_r = trunc_right(trunc_r)
-                    if trunc_l not in ls_primes:
-                        break
-                    if trunc_r not in ls_primes:
-                        break
-                    count += 1
-                if count == k - 1:
-                    self.ans += prime
-                    self.count += 1
-            if self.count == 11:  # only 11 of these primes exist
-                return self.ans
+            is_truncatable = True
+            for i in range(len(str(prime)) - 1):
+                trunc_l = trunc_left(trunc_l)
+                trunc_r = trunc_right(trunc_r)
+                if trunc_l not in set_primes:
+                    is_truncatable = False
+                    break
+                if trunc_r not in set_primes:
+                    is_truncatable = False
+                    break
+
+            if is_truncatable:
+                self.ans += prime
+
         return self.ans
 
 
