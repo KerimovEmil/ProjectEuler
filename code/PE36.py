7c554b5ab95906411496c2bf51654bca12b8289e
KerimovEmil <emilkerimov93@gmail.com>
2019-09-21 21:46:51 -0400

    sped up 36

--- code/PE36.py
+++ code/PE36.py
@@ -7,20 +7,16 @@ Find the sum of all numbers, less than one million, which are palindromic in bas
 
 ANSWER:
 872187
-Solve time ~1.3 seconds
+Solve time ~0.6 seconds
 """
 
 from util.utils import timeit
 import unittest
 
 
-def is_palindromic(num):  # TODO: consider moving to utils?
-    r = str(num)
-    if len(r) == 1:
-        return True
-    first = r[:int(len(r) / 2)]
-    second = r[-len(first):]
-    return first == second[::-1]
+def is_palindromic(input_string):
+    """Return True if input string is a palindromic"""
+    return input_string == input_string[::-1]
 
 
 class Problem36:
@@ -31,10 +27,9 @@ class Problem36:
     @timeit
     def solve(self):
         for i in range(self.max_value):
-            if is_palindromic(i):
+            if is_palindromic(str(i)):
                 if is_palindromic(bin(i)[2:]):
                     self.sum += i
-
         return self.sum
 
 
