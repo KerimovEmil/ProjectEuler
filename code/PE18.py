bbc270b47be9a553645fa595de69450b090803a1
KerimovEmil <emilkerimov93@gmail.com>
2019-10-05 14:16:56 -0700

    adding solution to pe18 and pe 67 (#44)
    * adding solution to pe18 and pe 67
    
    * addressing code review comments

---
+++ code/PE18.py
@@ -0,0 +1,53 @@
+"""
+PROBLEM
+
+By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from
+top to bottom is 23.
+
+Find the maximum total from top to bottom of the triangle in p018_triangle.txt
+
+ANSWER: 1074
+Solve time ~0.002 seconds
+Related Problems: 67
+"""
+
+from util.utils import timeit
+import unittest
+
+
+class Problem18:
+    def __init__(self, data):
+        self.data = data
+
+    def reduce_triangle(self):
+        """
+        This method replaces the last row of the triangle with the sum of that row and the maximum of the left
+        and right paths. This replacement happens for each row of the triangle starting from the bottom until only one
+        number is left.
+        """
+        for i in range(len(self.data[-1]) - 1):
+            best_path = max(self.data[-1][i], self.data[-1][i + 1])
+            self.data[-2][i] += best_path
+        del self.data[-1]
+
+    @timeit
+    def solve(self):
+        for row in range(len(self.data)-1):
+            self.reduce_triangle()
+
+        return self.data[0][0]
+
+
+class Solution18(unittest.TestCase):
+    def setUp(self):
+        with open(r'../problem_data/p018_triangle.txt') as f:
+            triangle = [[int(n) for n in s.split()] for s in f.readlines()]
+
+        self.problem = Problem18(triangle)
+
+    def test_solution(self):
+        self.assertEqual(1074, self.problem.solve())
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ code/PE67.py
@@ -0,0 +1,32 @@
+"""
+PROBLEM
+
+By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from
+top to bottom is 23.
+
+Find the maximum total from top to bottom of the triangle in p067.txt
+(a 15K text file containing a triangle with one-hundred rows.)
+
+ANSWER:
+7273
+Solve time ~0.007 seconds
+Related Problems: 18
+"""
+
+import unittest
+from code.PE18 import Problem18
+
+
+class Solution67(unittest.TestCase):
+    def setUp(self):
+        with open(r'../problem_data/p067_triangle.txt') as f:
+            triangle = [[int(n) for n in s.split()] for s in f.readlines()]
+
+        self.problem = Problem18(triangle)
+
+    def test_solution(self):
+        self.assertEqual(7273, self.problem.solve())
+
+
+if __name__ == '__main__':
+    unittest.main()
