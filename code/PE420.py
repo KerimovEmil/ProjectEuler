de8263383ab45b07a9f7e2fdb5d5986e2427a718
KerimovEmil <emilkerimov93@gmail.com>
2019-11-11 23:02:03 -0500

    second commit, much faster but not fast enough

--- code/PE420.py
+++ code/PE420.py
@@ -13,18 +13,26 @@ We can verify that F(50) = 7 and F(1000) = 1019.
 Find F(10^7).
 
 ANSWER:
-???
+??? (145159332)
 Solve time ~  seconds
 """
 
 from util.utils import timeit, sieve
 import unittest
+from math import gcd
 
+# https://en.wikipedia.org/wiki/Square_root_of_a_2_by_2_matrix
+# https://projecteuler.net/problem=420
 
 # --------------------------------------------------------
 # a:28, d:76, b:24, c:72, bc:1728, det_pos:12.0, det_neg:8.0
 # ai:4.0, di:8.0, bi:2.0, ci:6.0
 # ai:1.0, di:7.0, bi:3.0, ci:9.0
+
+
+def lcm(x, y):
+    return x*y // gcd(x, y)
+
 
 def is_square(n):
     return abs(int(n**0.5) - n**0.5) < 1e-13
@@ -116,78 +124,75 @@ class Problem420:
                             print(f"a_pos:{a_pos}, d_pos:{d_pos}, b_pos:{b_pos}, c_pos:{c_pos}")
                             print(f"a_neg:{a_neg}, d_neg:{d_neg}, b_neg:{b_neg}, c_neg:{c_neg}")
                             print(f"count: {self.count}")
-                            # if a_pos*d_pos != b_neg*c_neg and a==d:
-                            #     print("HHHHHHHH")
-                            # if a_neg*d_neg != b_pos*c_pos and a==d:
-                            #     print("HHHHHHHH2")
-    #                     if self.count != len(self.solution_set):
-    #                         print("HERE")
-    #                         print(len(self.solution_set))
-        # print(len(self.solution_set))
         return self.count
 
     @timeit
-    def simple_solve(self):
-        for a in range(1, self.n//2):
-            for bc2 in range(1, a):
-                lamb_1 = a + bc2
-                lamb_2 = a - bc2
-                if is_square(lamb_1) and is_square(lamb_2):
-                    self.count += 1
-                    print(f"a:{a}, d:{a}")
+    def solve_trace(self):
+        # count: 852240, trace: 148228
 
-        return self.count
-
-    @timeit
-    def solve_hermitian(self):
-        # Solve for cases of A = D and B = C
-        for a in range(2, self.n // 2):
-            for delta in range(1, a):
-                det_pos = (2*a + 2 * delta) ** 0.5
+        # ls_possible_delta = [i ** 2 for i in range(1, int(self.n ** 0.5))]
+        for trace in range(2, self.n):
+            for delta in range(1, trace//2 + trace % 2):  # (trace-2del >0),  trace > 2 del, del <trace/2
+                det_pos = (trace + 2 * delta) ** 0.5  # todo improve this, combine pos + neg search
                 if not is_int(det_pos):
                     continue
                 else:
                     det_pos = int(det_pos)
-                det_neg = (2*a - 2 * delta) ** 0.5
+                det_neg = (trace - 2 * delta) ** 0.5
                 if not is_int(det_neg):
                     continue
                 else:
                     det_neg = int(det_neg)
+                det_pos_neg = lcm(det_pos, det_neg)
 
-                if (a + delta) % det_pos != 0:
-                    continue
-                if (a - delta) % det_neg != 0:
-                    continue
+                # a = -delta + k1*det_pos = delta + k2*det_neg
+                for a in range(delta + det_neg, trace // 2 + 1, det_neg):  # a - delta > 0
+                    d = trace - a  # checks on d are true automatically since (a+delta + d+delta)/det_pos = int
+                    if (a + delta) % det_pos != 0:
+                        # print("a_check")
+                        continue
+                    # if (a - delta) % det_neg != 0:
+                    #     print("a_check")
+                    #     continue
 
-                bc = int(a * a - delta ** 2)
-                if bc % (det_pos ** 2) != 0:
-                    continue
-                if is_int(bc**0.5):
-                    b = int(bc**0.5)
-                    if b % det_pos != 0:
-                        continue
-                    if b % det_neg != 0:
-                        continue
-                    self.count += 1
+                    bc = int(a * d - delta ** 2)
+                    # if bc % (det_pos_neg ** 2) != 0:  # bc check not needed
+                    #     print("bc_hello")
+                    #     continue
+
+                    bc_prime = bc // (det_pos_neg ** 2)
+                    bc_count = 0
+                    bc_limit = int(bc_prime ** 0.5)
+                    if is_int(bc_prime ** 0.5):  # b == c
+                        bc_count += 1
+                    else:
+                        bc_limit += 1
+                    bc_count += 2 * sum(bc_prime % i == 0 for i in range(1, bc_limit))
+
+                    if a != d:
+                        self.count += 2 * bc_count
+                    else:
+                        self.count += bc_count
+
                     if self.debug:
+                        # b = i * det_pos_neg
+                        b = 0
+                        # c = j * det_pos_neg
+                        c = 0
                         a_neg = (a - delta) // det_neg
-                        d_neg = (a - delta) // det_neg
-                        d_pos = (a + delta) // det_pos
+                        d_neg = (d - delta) // det_neg
+                        d_pos = (d + delta) // det_pos
                         a_pos = (a + delta) // det_pos
                         b_pos = b // det_pos
-                        c_pos = b // det_pos
+                        c_pos = c // det_pos
                         b_neg = b // det_neg
-                        c_neg = b // det_neg
+                        c_neg = c // det_neg
                         print("--------------------------------------------------------")
-                        print(
-                            f"a:{a}, d:{a}, b:{b}, c:{b}, bc:{bc}, det_pos:{det_pos}, det_neg:{det_neg}, delta:{delta}")
+                        print(f"a:{a}, d:{d}, b:{b}, c:{c}, bc:{bc}, det_pos:{det_pos}, det_neg:{det_neg}, delta:{delta}")
+                        print(f'bc_prime:{bc_prime}, bc_limit:{bc_limit}')
                         print(f"a_pos:{a_pos}, d_pos:{d_pos}, b_pos:{b_pos}, c_pos:{c_pos}")
                         print(f"a_neg:{a_neg}, d_neg:{d_neg}, b_neg:{b_neg}, c_neg:{c_neg}")
-                        print(f"count: {self.count}")
-    #                     if self.count != len(self.solution_set):
-    #                         print("HERE")
-    #                         print(len(self.solution_set))
-        # print(len(self.solution_set))
+                        print(f"count: {self.count}, trace:{trace}")
         return self.count
 
 
@@ -198,52 +203,21 @@ class Solution420(unittest.TestCase):
     def test_solution(self):
         self.assertEqual(7, Problem420(n=50, debug=True).solve())
 
-    def test_solution_herm(self):
-        self.assertEqual(177, Problem420(n=1000, debug=True).solve_hermitian())
+    # def test_solution_herm(self):
+    #     self.assertEqual(177, Problem420(n=1000, debug=True).solve_hermitian())
 
-    # def test_solution_herm2(self):
-    #     self.assertEqual(177, Problem420(n=10000000, debug=True).solve_hermitian())
+    def test_solution_trace(self):
+        self.assertEqual(1019, Problem420(n=1000, debug=True).solve_trace())
 
-    def test_solution2(self):
-        self.assertEqual(1019, Problem420(n=1000, debug=True).solve())
-    #
-    # def test_solution3(self):
-    #     self.assertEqual(None, Problem420(n=10000000, debug=True).solve())
+    # def test_solution2(self):
+    #     self.assertEqual(1019, Problem420(n=1000, debug=True).solve())
+
+    def test_solution_trace3(self):
+        self.assertEqual(16021, Problem420(n=7000, debug=True).solve_trace())
+
+    # def test_solution_trace4(self):
+    #     self.assertEqual(None, Problem420(n=10000000, debug=True).solve_trace())
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
-# --------------------------------------------------------
-# a:496, d:304, b:96, c:1440, bc:138240, det_pos:32.0, det_neg:24.0, delta:112
-# ai:19.0, di:13.0, bi:3.0, ci:45.0
-# ai:16.0, di:8.0, bi:4.0, ci:60.0
-# count: 1523
-# --------------------------------------------------------
-# a:496, d:304, b:288, c:480, bc:138240, det_pos:32.0, det_neg:24.0, delta:112
-# ai:19.0, di:13.0, bi:9.0, ci:15.0
-# ai:16.0, di:8.0, bi:12.0, ci:20.0
-# count: 1527
-
-import sympy
-
-correct_matrix = sympy.Matrix([[496, 96], [1440, 304]])
-sympy.Matrix([[19, 3], [45, 13]])**2
-sympy.Matrix([[16, 4], [60, 8]])**2
-
-# change b and c
-correct_matrix = sympy.Matrix([[496, 1440], [96, 304]])
-sympy.Matrix([[19, 45], [3, 13]])**2
-sympy.Matrix([[16, 60], [4, 8]])**2
-
-# change a and d
-correct_matrix = sympy.Matrix([[304, 96], [1440, 496]])
-sympy.Matrix([[13, 3], [45, 19]])**2
-sympy.Matrix([[8, 4], [60, 16]])**2
-
-# change a and d and change b and c
-correct_matrix = sympy.Matrix([[304, 1440], [96, 496]])
-sympy.Matrix([[13, 45], [3, 19]])**2
-sympy.Matrix([[8, 60], [4, 16]])**2
