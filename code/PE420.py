cc762c60b58f57ce290ea17817c547670d66f08f
KerimovEmil <emilkerimov93@gmail.com>
2019-11-12 22:45:29 -0500

    minor hacks, but got the solution finally

--- code/PE420.py
+++ code/PE420.py
@@ -14,7 +14,7 @@ Find F(10^7).
 
 ANSWER:
 145159332
-Solve time ~  seconds
+Solve time ~1113 seconds
 """
 
 from util.utils import timeit
@@ -41,27 +41,42 @@ class Problem420:
 
     @timeit
     def solve(self):
+        ls_p = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103,
+                107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211,
+                223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331,
+                337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443,
+                449, 457, 461, 463, 467, 479, 487, 491, 499]
+
         # det_neg < det_pos
         # det_neg^2 + det_pos^2 = 2*trace < 2*n
         # det_neg < sqrt(2*n)
         # det_pos < sqrt(2*n - det_neg^2)
-        for det_neg in range(1, int((2*self.n) ** 0.5)):
+        # for det_neg in range(1, int((2*self.n) ** 0.5)):
+        for det_neg in range(2, int((2*self.n) ** 0.5)):
             d_pos_limit = (2*self.n - det_neg ** 2)**0.5
             if is_int(d_pos_limit):
                 d_pos_limit = int(d_pos_limit)
             else:
                 d_pos_limit = int(d_pos_limit) + 1
 
-            for det_pos in range(det_neg+2, d_pos_limit, 2):
+            if det_neg in ls_p:
+                det_pos_range = range(2*det_neg, d_pos_limit, det_neg)
+            else:
+                det_pos_range = range(det_neg + 2, d_pos_limit, 2)
+
+            # for det_pos in range(det_neg+2, d_pos_limit, 2):
+            for det_pos in det_pos_range:
                 trace = (det_pos**2 + det_neg**2)//2
-                # delta = (det_pos**2 - det_neg**2)//2
-                delta = int((trace - det_neg**2)/2)
+                delta = (det_pos**2 - det_neg**2)//4  # delta = int((trace - det_neg**2)/2)
 
                 det_pos_neg = lcm(det_pos, det_neg)
+                # print(f'det_pos_neg:{det_pos_neg}, det_neg:{det_neg}, det_pos:{det_pos}')
 
                 # a = -delta + k1*det_pos = delta + k2*det_neg
                 for a in range(delta + det_neg, trace // 2 + 1, det_neg):  # a - delta > 0
+                # todo A must be a multiple of det_pos_neg apparently
                     if (a + delta) % det_pos != 0:
+                        # print(f'det_pos_neg:{det_pos_neg}, det_neg:{det_neg}, det_pos:{det_pos}')
                         continue
 
                     d = trace - a  # checks on d are true automatically since (a+delta + d+delta)/det_pos = int
@@ -82,15 +97,15 @@ class Problem420:
                         self.count += bc_count
 
                     if self.debug:
-                        a_neg = (a - delta) // det_neg
-                        d_neg = (d - delta) // det_neg
-                        d_pos = (d + delta) // det_pos
-                        a_pos = (a + delta) // det_pos
-                        print("--------------------------------------------------------")
-                        print(f"a:{a}, d:{d}, bc:{bc}, det_pos:{det_pos}, det_neg:{det_neg}, delta:{delta}")
-                        print(f'bc_prime:{bc_prime}, bc_limit:{bc_limit}')
-                        print(f"a_pos:{a_pos}, d_pos:{d_pos}")
-                        print(f"a_neg:{a_neg}, d_neg:{d_neg}")
+                        # a_neg = (a - delta) // det_neg
+                        # d_neg = (d - delta) // det_neg
+                        # d_pos = (d + delta) // det_pos
+                        # a_pos = (a + delta) // det_pos
+                        # print("--------------------------------------------------------")
+                        # print(f"a:{a}, d:{d}, bc:{bc}, det_pos:{det_pos}, det_neg:{det_neg}, delta:{delta}")
+                        # print(f'bc_prime:{bc_prime}, bc_limit:{bc_limit}')
+                        # print(f"a_pos:{a_pos}, d_pos:{d_pos}")
+                        # print(f"a_neg:{a_neg}, d_neg:{d_neg}")
                         print(f"count: {self.count}, trace:{trace}")
         return self.count
 
