e5efad02128ed7136b6e5c549615dbb1fc494f8f
KerimovEmil <emilkerimov93@gmail.com>
2020-04-04 00:00:42 -0400

    adding faster solution to pe432 and a new util function

--- solutions/PE432.py
+++ solutions/PE432.py
@@ -9,85 +9,63 @@ Give the last 9 digits of your answer.
 
 ANSWER:
 754862080
-Solve time too_long seconds
+Solve time 618 seconds ~ 10 mins + 20 seconds
 """
 
 import unittest
-from util.utils import timeit, euler_totient_function, len_faray_seq
-from math import gcd
+from util.utils import timeit, euler_totient_function, sum_phi
 
 # 510510 = 2×3×5×7×11×13×17
 # n = 510510
 # phi(n) = 92160
 
 
-@timeit
-def g(m):
-    """For any m,n>0, let d=gcd(m,n), then Phi(m*n) = Phi(m) * Phi(n) * d/Phi(d)"""
-    n = 510510
-    ans = 0
-    phi_n = euler_totient_function(n)
-
-    for i in range(1, m+1):
-        d = gcd(i, n)
-        ans += euler_totient_function(i) * d / euler_totient_function(d)
-    return ans*phi_n
-
-
-def factor(n):
-    """returns the largest factor of n"""
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return factor(n // i)
-    return n
-
-# def factor_2(n):
-#     """returns the largest factor of n"""
-#     for i in range(int(n ** 0.5) + 1, 2, -1):
-#         if n % i == 0:
-#             return i
-#     return None
-
-
-
-def S(n, m):
-    # print(n, m)
-    if m == 0 or n == 0:
-        return 0
-    if m == 1:
-        return euler_totient_function(n)
-    if n == 1:
-        return len_faray_seq(m) - 1
-
-    p = factor(n)
-    q = n // p
-    r = (p - 1) * S(q, m)
-    return r + S(n, m // p)
+# Solution idea:
+# comment from user Guego on https://projecteuler.net/thread=432
+# It is easy to compute S(1,n) recursively using the relation S(1,n) = n(n+1)/2 − ∑d≥2 S(1,n/d).
+# Then, I just noticed that S(p*k,n)=(p−1)*S(k,n)+S(p*k,n/p) if p is a prime number.
+# S(1,n) = sum of euler totient function, could be implemented even more efficiently, see util function
 
 
 class Problem432:
-    def __init__(self, n):
+    def __init__(self, n, ls_p, mod):
         self.n = n
+        self.ls_p = ls_p
+        self.mod = mod
 
     @timeit
     def solve(self, m):
-        return S(self.n, m)
+
+        def S(L, n):
+            if len(L) == 0:
+                return sum_phi(n)
+            else:
+                if n == 0:
+                    return 0
+                else:
+                    return (L[0] - 1) * S(L[1:], n) + S(L, n // L[0])
+
+        return S(self.ls_p, m) % self.mod
 
 
 class Solution432(unittest.TestCase):
-    def setUp(self):
-        self.problem = Problem432(n=510510)
+    @classmethod
+    def setUpClass(cls):
+        cls.problem = Problem432(n=510510, ls_p=[2, 3, 5, 7, 11, 13, 17], mod=int(1e9))
 
     def test_solution(self):
-        self.assertEqual(754862080, self.problem.solve(int(1e11)) % int(1e9))
+        self.assertEqual(754862080, self.problem.solve(int(1e11)))
+
+    def test_sum_phi_function(self):
+        n = 5000
+        self.assertEqual(sum(euler_totient_function(i) for i in range(1, n+1)), sum_phi(n))
 
     def test_solution_4(self):
-        self.assertEqual(4548570531840, self.problem.solve(int(1e4)))
+        self.assertEqual(570531840, self.problem.solve(int(1e4)))
 
     def test_solution_6(self):
-        self.assertEqual(45480596821125120, self.problem.solve(int(1e6)))
+        self.assertEqual(821125120, self.problem.solve(int(1e6)))
 
 
 if __name__ == '__main__':
     unittest.main()
-
