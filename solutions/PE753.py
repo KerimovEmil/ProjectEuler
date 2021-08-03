9dbeb8f0d2522e70375aacfdd8828a5fc3d9d1a7
KerimovEmil <emilkerimov93@gmail.com>
2021-08-03 00:02:10 -0400

    753: come code cleanup

--- solutions/PE753.py
+++ solutions/PE753.py
@@ -1,144 +1,18 @@
 from util.utils import timeit
 import unittest
-from numpy import pi, exp, arange, cos
 from primesieve import primes
-import numpy as np
-
-def r(a, p):
-    np_range = arange(1, p//2 + 1)
-    return cos(2*pi/p * a * np_range**3)
-
-# returns -0.5
-# np.array([r(a, 13) for a in range(1, 13//2 + 1)]).sum(axis=0)
-
-
-# for p in primes(3, 100):
-#     if p % 3 == 1:
-#         p_pi_2 = 2 * pi / p
-#         np_range = arange(1, p // 2 + 1)
-#
-#         f = -(16 * sum(sum(cos(p_pi_2 * a * np_range ** 3)) ** 3 for a in np_range) / (p - 1) + 1) / p
-#         form_f = int(round(f, 4))
-#         print(p, form_f, given_p(p), p ** 2 - 3 * p + 2 - form_f * (p - 1))
-
-
-# 7 5 2
-# 13 11 4
-# 19 -1 6
-# 31 2 10
-# 37 17 12
-# 43 14 14
-# 61 5 20
-# 67 11 22
-# 73 -1 24
-# 79 23 26
-# 97 -13 32
-
-
-# for p in primes(3,100):
-#     if p%3 == 1:
-#         p_pi_2 = 2 * pi/p
-#         np_range = arange(1, p//2 + 1)
-#         cos_sum = 16 * sum(sum(cos(p_pi_2 * a * np_range ** 3)) ** 3 for a in np_range)
-#         f = -(cos_sum / (p - 1) + 1) / p
-#         form_f = int(round(f, 4))
-#         form_cossum_3 = int(round(cos_sum/3, 4))
-#         print(p, form_f, form_f+p, (form_f+p)//3,(p-1)/3 , given_p(p), p**2- 3*p + 2 - form_f*(p-1), cos_sum, cos_sum/(p-1)/3/3)
-
-
-# def int_print(*args):
-#     ls = []
-#     for x in args:
-#         if abs(int(round(x, 4)) - x) <= 1e-3:
-#             ls.append(int(round(x, 4)))
-#         else:
-#             raise NotImplementedError(f'{x} is not an integer')
-#     print(ls)
-#
-# for p in primes(3,100):
-#     if p%3 ==1:
-#         np_range = arange(1, p//2 + 1)
-#         cos_sum = 16 * sum(sum(cos(2 * pi/p * a * np_range ** 3)) ** 3 for a in np_range)
-#         f = -(cos_sum / (p - 1) + 1) / p
-#         int_print(p, given_p(p), f, cos_sum, cos_sum/(p-1)/9)
-
-# for p in primes(3,200):
-#     if p%3 ==1:
-#         np_range = arange(1, p//2 + 1)
-#         cos_sum = 16 * sum(sum(cos(2 * pi/p * a * np_range ** 3)) ** 3 for a in np_range)
-#         f = -cos_sum / (p - 1)/p - 1/ p
-#         g_p = (p-1)**3/p + cos_sum/p
-#         int_print(p, f, cos_sum/(p-1)/9 + 2*p, g_p/(p-1)/9, (p//3)/2)
-
-
-def is_int(n):
-    return abs(n - int(n)) < 1e-13
-
-def find_a(p):
-    """Find a such that 4p = a^2 + 27b^2 and a==1 mod 3"""
-    p4 = 4*p
-    max_b2 = p4 // 27
-    for i in range(int(max_b2**0.5)+3):
-        if i**2 > max_b2:
-            b = i
-            break
-
-    for _ in range(b+1):
-        b -= 1
-        a2 = p4 - 27*b**2
-        if is_int(round(a2**0.5, 6)):
-            a = int(round(a2**0.5, 4))
-            break
-
-    if a%3 == 1:
-        return a
-    elif (-a) %3 == 1:
-        return -a
-    else:
-        raise NotImplementedError(a)
 
 
 class Problem753:
-    def __init__(self):
-        pass
+    def __init__(self, max_p):
+        self.dc = self.create_dict(max_p)
+        self.max_p = max_p
 
-    @staticmethod
-    def given_p_simple(p: int) -> int:
+    def given_p(self, p: int) -> int:
         if p % 3 != 1:
-            return (p-1)*(p-2)
-
-        dc_c_values = {}
-        for i in range(1, p):
-            i3 = i ** 3 % p
-            dc_c_values[i3] = 1 + dc_c_values.get(i3, 0)
-
-        ans = 0
-
-        for i in range(1, p):
-            cubed_sum = (2*i**3) % p
-            ans += dc_c_values.get(cubed_sum, 0)
-
-        for i in range(1, p):
-            for j in range(i+1, p):
-                cubed_sum = (i**3 + j**3) % p
-                ans += 2*dc_c_values.get(cubed_sum, 0)
-
-        return ans
-
-    @staticmethod
-    def given_p_simple_2(p: int) -> int:
-        """
-        Only called when p%3 == 1.
-        Returns (p-1)* number of solutions to (a^3 + 1 = c^3)
-        """
-
-        set_c_values = {i ** 3 % p for i in range(1, p)}  # there are 3 values of i for each one value of i^3
-        # if p==1 mod 3, then a is a cube mod p, iff a**((p-1)/2) == 1
-        # how many a and a+1 are cubes
-        # therefore a**((p-1)/2) == 1, and (a+1)**((p-1)/2) == 1
-        ans = sum((i3 + 1) in set_c_values for i3 in set_c_values)
-
-        return ans
+            return (p-1) * (p-2)
+        else:
+            return self.given_p_3(p)
 
     def given_p_3(self, p: int) -> int:
         """
@@ -149,68 +23,14 @@ class Problem753:
         Note that this only works when p==1 mod 3
         See paper here: http://matwbn.icm.edu.pl/ksiazki/aa/aa37/aa3718.pdf
         """
-        # return find_a(p)+p-8
-        return self.dc[p]+p-8
+        return (self.dc[p]+p-8)*(p-1)
 
     @staticmethod
-    def given_p(p: int) -> int:
-        if p % 3 != 1:
-            return (p-1)*(p-2)
-
-        # given_p must be divisible by (p-1)
-
-        # therefore p-1 must be divisible by 3
-        # therefore cos_sum must be divisible by 3
-
-        # magic_number = None
-        # return (p**2 - 3*p + 2) - magic_number*(p-1)
-        # return p(p - 3) + 2 - magic_number*(p-1)
-
-        p_pi_2 = 2 * pi/p
-        np_range = arange(1, p//2 + 1)
-
-        ans = (p-1)**3
-        ans += 16*sum(sum(cos(p_pi_2 * a * np_range**3))**3 for a in np_range)
-        # cos sum is divisible by 9*(p-1)
-
-        # (-16*cos_sums/(p-1) -1) /p must be an integer
-
-        # for 37, = (-0.5) * len(np_range) * (37//3) * 210
-        # for 37, = (-0.5) * len(np_range) * 2520
-        # for 43, = (-0.5) * len(np_range) * 2412
-        # for 43, final answer is 1134
-        # for 43, 1134 = ((43-1)^3 + (-0.5) * len(np_range) * 2412) / 43
-        # for 43, 1134 = (43^3 - 3*43^2 + 3*43 -1 + (-0.5) * len(np_range) * 2412) / 43
-        # for 43, 1134 = (43^2 - 3*43 + 3) + (-1 + (-0.5) * len(np_range) * 2412) / 43
-        # for 43, 1134 = (43^2 - 3*43 + 3) + (-1 + (-0.5) * (43-1)/2 * 2412) / 43
-        # for 43, 1134 = (43^2 - 3*43 + 3) + (-1 - (43-1)/4 * 2412) / 43
-        # for 43, 1134 = (43^2 - 3*43 + 3) + (-1 - (43-1) * 603) / 43
-        # for 43, 1134 = (43^2 - 3*43 + 3) + (-(43-1) * 603 - 1) / 43
-        # for 43, 1134 = (43^2 - 3*43 + 3) + (-43* 603 + 1*603 - 1) / 43
-        # for 43, 1134 = (43^2 - 3*43 + 3) - 603 + (603 - 1) / 43
-        #  (603 - 1) / 43 = 14 = 43 // 3
-        # for 43, 1134 = (43**2 - 3*43 + 3) - (43*(43//3) +1) + (43//3)
-
-        # for 37, final answer is 648
-        # for 37, 648 = ((37-1)**3 + (-0.5) * (37-1)/2 * 2520) / 37
-        # for 37, 648 = ((37-1)**3 - (37-1) * 630) / 37
-        # for 37, 648 = (37**2 - 3*37 + 3) + (-(37-1) * 630 - 1) / 37
-        # for 37, 648 = (37**2 - 3*37 + 3) - 630 + (630 - 1) / 37
-        # (630 - 1) / 37 = 17
-
-        # [a, b, c]
-        # [d, e, f]
-        # [g, h, i]
-
-        # sum = (a+b+c)^3 + (d+e+f)^3 + (g+h+i)^3
-        # we know that (a+d+g) = -0.5, and (b+e+h) = -0.5, and (g+h+i) = -0.5
-
-        return int(round((ans/p), 4))
-
-    def create_dict(self, max_p):
-
-        # 4p = a^2 + 27 * b^2
-        # max p = 6,000,000
+    def create_dict(max_p):
+        """
+        Create dictionary of unique values of a for each p (p==1 mod 3), such that
+         4p = a^2 + 27b^2, and a==1 mod 3
+         """
         max_b = int((4*max_p//27)**0.5) + 10
         max_a = int(((4*max_p)**0.5)) + 10
 
@@ -222,50 +42,40 @@ class Problem753:
 
         assert (-max_a) % 3 == 1
 
-        self.dc = {}
+        dc = {}
         for a in range(-max_a, max_a, 3):  # a == 1 mod 3
             for b in range(1, max_b):
                 p4 = a ** 2 + 27 * (b ** 2)
                 if p4 % 4 == 0:
-                    self.dc[p4 // 4] = a
+                    dc[p4 // 4] = a
+        return dc
 
     @timeit
-    def solve(self, max_p: int) -> int:
-
-        self.create_dict(max_p)
+    def solve(self, max_p: int = None) -> int:
+        if max_p is None:
+            max_p = self.max_p
 
         ans = sum((p-1)*(p-2) for p in primes(max_p) if p % 3 != 1)
 
         mod_3_1_primes = (p for p in primes(max_p) if p % 3 == 1)
-        print('starting calculating p==1 mod 3')
 
-        # ans += 9 * sum((p-1)*self.given_p_simple_2(p) for p in mod_3_1_primes)
-        ans += sum((p-1)*self.given_p_3(p) for p in mod_3_1_primes)
+        ans += sum(self.given_p_3(p) for p in mod_3_1_primes)
         return ans
 
 
 class Solution753(unittest.TestCase):
     def setUp(self):
-        self.problem = Problem753()
+        self.problem = Problem753(max_p=6000000)
 
     def test_specific_p(self):
         with self.subTest('testing p=5'):
             self.assertEqual(12, self.problem.given_p(5))
-            # self.assertEqual(12, self.problem.given_p_simple(5))
         with self.subTest('testing p=7'):
             self.assertEqual(0, self.problem.given_p(7))
-            # self.assertEqual(0, self.problem.given_p_simple(7))
 
     def test_solution(self):
-        self.assertEqual(4714126766770661630, self.problem.solve(6000000))
-        # self.assertEqual(3259631135871932, self.problem.solve(500000))
-        # self.assertEqual(29820865379858, self.problem.solve(100000))
-        # self.assertEqual(3964606054604, self.problem.solve(50000))
-        # self.assertEqual(37501868762, self.problem.solve(10000))
-        # self.assertEqual(5041836452, self.problem.solve(5000))
-        # self.assertEqual(48911172, self.problem.solve(1000))
-        # self.assertEqual(6910616, self.problem.solve(500))
-        # self.assertEqual(59762, self.problem.solve(100))
+        self.assertEqual(4714126766770661630, self.problem.solve())
+        self.assertEqual(59762, self.problem.solve(100))
 
 
 if __name__ == '__main__':
