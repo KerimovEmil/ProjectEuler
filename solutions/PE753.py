0ac3d5a48787985fa7bad7ce26d3fbeb2e562cef
KerimovEmil <emilkerimov93@gmail.com>
2021-08-03 00:05:48 -0400

    753: added file doc string

--- solutions/PE753.py
+++ solutions/PE753.py
@@ -1,3 +1,20 @@
+"""
+Fermat's Last Theorem states that no three positive integers a,b,c satisfy the equation a^n + b^n = c^n
+for any integer value of n greater than 2.
+
+For this problem we are only considering the case n=3. For certain values of p, it is possible to solve the congruence equation:
+a^3 + b^3 = c^3 mod p
+
+For a prime p, we define F(p) as the number of integer solutions to this equation for 1 <= a,b,c < p.
+
+You are given F(5)=12 and F(7)=0.
+
+Find the sum of F(p) over all primes p less than 6,000,000.
+
+ANSWER: 4714126766770661630
+Solve time ~4.5 seconds
+"""
+
 from util.utils import timeit
 import unittest
 from primesieve import primes
