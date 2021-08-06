442ed2c333413a32e360a6af38b8c269bbc681e3
KerimovEmil <emilkerimov93@gmail.com>
2021-08-06 18:12:28 -0400

    fixing new folder name and addressing some more pylint stuff

--- solutions/PE753.py
+++ solutions/PE753.py
@@ -43,7 +43,7 @@ class Problem753:
     def given_p(self, p: int) -> int:
         """Return the number of solutions to a^3 + b^3 = c^3 mod p for 1 <= a,b,c < p"""
         if p % 3 != 1:
-            return (p-1) * (p-2)
+            return (p - 1) * (p - 2)
         else:
             return self.given_p_3(p)
 
@@ -56,7 +56,7 @@ class Problem753:
         Note that this only works when p==1 mod 3
         See paper here: http://matwbn.icm.edu.pl/ksiazki/aa/aa37/aa3718.pdf
         """
-        return (p-1) * (self.dc[p]+p-8)
+        return (p - 1) * (self.dc[p] + p - 8)
 
     @staticmethod
     def create_dict(max_p):
@@ -64,12 +64,12 @@ class Problem753:
         Create dictionary of unique values of a for each p (p==1 mod 3), such that
          4p = a^2 + 27b^2, and a==1 mod 3
          """
-        max_b = int((4*max_p//27)**0.5) + 10
-        max_a = int(((4*max_p)**0.5)) + 10
+        max_b = int((4 * max_p // 27) ** 0.5) + 10
+        max_a = int(((4 * max_p) ** 0.5)) + 10
 
         # make sure the first value of a is 1 mod 3
         x = (-max_a) % 3
-        max_a += (x-1) % 3
+        max_a += (x - 1) % 3
         assert (-max_a) % 3 == 1
 
         dc = {}
@@ -85,7 +85,7 @@ class Problem753:
         if max_p is None:
             max_p = self.max_p
 
-        ans = sum((p-1)*(p-2) for p in primes(max_p) if p % 3 != 1)
+        ans = sum((p - 1) * (p - 2) for p in primes(max_p) if p % 3 != 1)
 
         mod_3_1_primes = (p for p in primes(max_p) if p % 3 == 1)
 
@@ -111,4 +111,3 @@ class Solution753(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
