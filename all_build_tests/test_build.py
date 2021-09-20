ebbe4d266c6e6fcce49ae65e0b23eaf1651c50ba
KerimovEmil <emilkerimov93@gmail.com>
2021-09-20 00:05:48 -0400

    added solution to pe 116

--- all_build_tests/test_build.py
+++ all_build_tests/test_build.py
@@ -1,8 +1,10 @@
 from solutions.PE753 import Solution753
 from solutions.PE1 import Solution1
 from solutions.PE90 import Solution90
+from solutions.PE116 import Solution116
 
 if __name__ == '__main__':
     Solution1.main()
     Solution753.main()
     Solution90.main()
+    Solution116.main()
