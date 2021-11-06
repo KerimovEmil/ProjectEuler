8be55194929eea204e83634cdca80212ac779c22
KerimovEmil <emilkerimov93@gmail.com>
2021-11-06 12:43:06 -0400

    fixed unittests to be more general

--- all_build_tests/test_build.py
+++ all_build_tests/test_build.py
@@ -1,16 +1,23 @@
-from solutions.PE753 import Solution753
-from solutions.PE1 import Solution1
-from solutions.PE90 import Solution90
-from solutions.PE116 import Solution116
-
 import unittest
 from importlib import import_module
-# x = import_module('solutions.PE1', 'Solution1')
+
+solved_quick_problems = [1, 2, 3, 4, 5, 6, 10, 16, 18, 30, 90, 116, 486, 753]
+
+
+def suite():
+    """
+        Gather all the tests from this module in a test suite.
+    """
+    test_suite = unittest.TestSuite()
+    loader = unittest.TestLoader()
+    for prob in solved_quick_problems:
+        _mod = import_module(f'solutions.PE{prob}')
+        test_suite.addTests(loader.loadTestsFromModule(_mod))
+    return test_suite
 
 
 if __name__ == '__main__':
-    unittest.main()
-    # Solution1.main()
-    # Solution753.main()
-    # Solution90.main()
-    # Solution116.main()
+    mySuite = suite()
+
+    runner = unittest.TextTestRunner()
+    runner.run(mySuite)
