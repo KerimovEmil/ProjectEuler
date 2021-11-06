c1db48f368496043187665e3e9cfdec47d298d71
KerimovEmil <emilkerimov93@gmail.com>
2021-11-06 13:03:47 -0400

    removed use of testsuite

--- all_build_tests/test_build.py
+++ all_build_tests/test_build.py
@@ -3,21 +3,10 @@ from importlib import import_module
 
 solved_quick_problems = [1, 2, 3, 4, 5, 6, 10, 16, 18, 30, 90, 116, 486, 753]
 
-
-def suite():
-    """
-        Gather all the tests from this module in a test suite.
-    """
-    test_suite = unittest.TestSuite()
-    loader = unittest.TestLoader()
-    for prob in solved_quick_problems:
-        _mod = import_module(f'solutions.PE{prob}')
-        test_suite.addTests(loader.loadTestsFromModule(_mod))
-    return test_suite
+for prob in solved_quick_problems:
+    _mod = import_module(f'solutions.PE{prob}')
+    globals()[f'Solution{prob}'] = getattr(_mod, f'Solution{prob}')
 
 
 if __name__ == '__main__':
-    mySuite = suite()
-
-    runner = unittest.TextTestRunner()
-    runner.run(mySuite)
+    unittest.main()
