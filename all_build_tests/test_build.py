d4a8b2984ae23e801a57c6d99e25eae59f90b96e
KerimovEmil <emilkerimov93@gmail.com>
2021-11-11 16:05:40 -0500

    added more problems to unittests, and sped up 187

--- all_build_tests/test_build.py
+++ all_build_tests/test_build.py
@@ -3,8 +3,9 @@ from importlib import import_module
 
 solved_quick_problems = [1, 2, 3, 4, 5, 6, 10, 16, 18, 30, 32, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 45, 47, 48, 49,
                          50, 51, 53, 54, 55, 56, 57, 60, 63, 65, 66, 67, 69, 72, 73, 76, 81, 82, 89, 90, 92, 97, 99,
-                         116, 144, 486, 753]
-bad_or_slow_solutions = [46, 96]  # todo make this the final list to maintain eventually
+                         101, 108, 110, 111, 116, 120, 124, 125, 137, 139, 140, 144, 145, 187,
+                         486, 753]
+bad_or_slow_solutions = [46, 96, 179, 185]  # todo make this the final list to maintain eventually
 
 for prob in solved_quick_problems:
     _mod = import_module(f'solutions.PE{prob}')
