6fe0e12c7ce36a63f493bb481a20358d0e2726fa
EmilKerimov <emil.kerimov93@gmail.com>
2022-02-06 17:51:00 -0500

    added correct format of code

--- solutions/PE185_simulated_annealing.py
+++ solutions/PE185_simulated_annealing.py
@@ -1,16 +1,57 @@
-# Project Euler Problem 185
+"""
+PROBLEM
 
-# 90342 ;2 correct
-# 70794 ;0 correct
-# 39458 ;2 correct
-# 34109 ;1 correct
-# 51545 ;2 correct
-# 12531 ;1 correct
+The game Number Mind is a variant of the well known game Master Mind.
+Instead of coloured pegs, you have to guess a secret sequence of digits.
+After each guess you're only told in how many places you've guessed the correct digit.
+So, if the sequence was 1234 and you guessed 2036, you'd be told that you have one correct digit;
+however, you would NOT be told that you also have another digit in the wrong place.
 
-# ans = 39542
+For instance, given the following guesses for a 5-digit secret sequence,
 
-# ANSWER
-# 4640261571849533
+90342 ;2 correct
+70794 ;0 correct
+39458 ;2 correct
+34109 ;1 correct
+51545 ;2 correct
+12531 ;1 correct
+
+The correct sequence 39542 is unique.
+
+Based on the following guesses,
+
+5616185650518293 ;2 correct
+3847439647293047 ;1 correct
+5855462940810587 ;3 correct
+9742855507068353 ;3 correct
+4296849643607543 ;3 correct
+3174248439465858 ;1 correct
+4513559094146117 ;2 correct
+7890971548908067 ;3 correct
+8157356344118483 ;1 correct
+2615250744386899 ;2 correct
+8690095851526254 ;3 correct
+6375711915077050 ;1 correct
+6913859173121360 ;1 correct
+6442889055042768 ;2 correct
+2321386104303845 ;0 correct
+2326509471271448 ;2 correct
+5251583379644322 ;2 correct
+1748270476758276 ;3 correct
+4895722652190306 ;1 correct
+3041631117224635 ;3 correct
+1841236454324589 ;3 correct
+2659862637316867 ;2 correct
+
+Find the unique 16-digit secret sequence.
+
+ANSWER: 4640261571849533
+
+Solve time ~TOO LONG seconds
+"""
+
+import unittest
+from util.utils import timeit
 import random
 
 
@@ -70,6 +111,7 @@ class Problem185:
 
         return best_guess, min_dist
 
+    @timeit
     def solve(self):
         min_dist = self.digits * len(self.ls_attempts)
         is_solved = False
@@ -82,48 +124,54 @@ class Problem185:
                 return best_guess
 
 
-def main1():
-    test_list = [90342, 70794, 39458, 34109, 51545, 12531]
-    test_list = [str(x) for x in test_list]
-    correct_list = [2, 0, 2, 1, 2, 1]
-    ls_attempts = list(zip(test_list, correct_list))
-    a = Problem185(ls_attempts)
-    sol = a.solve()
-    print(sol)
+class Solution185(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_solution_small(self):
+
+        ls_attempts = [
+            (90342, 2),
+            (70794, 0),
+            (39458, 2),
+            (34109, 1),
+            (51545, 2),
+            (12531, 1),
+        ]
+        ls_attempts_str = [(str(x), y) for (x, y) in ls_attempts]
+        problem = Problem185(ls_attempts_str)
+        self.assertEqual(39542, problem.solve())
+
+    def test_solution(self):
+        ls_attempts = [
+            (5616185650518293, 2),
+            (3847439647293047, 1),
+            (5855462940810587, 3),
+            (9742855507068353, 3),
+            (4296849643607543, 3),
+            (3174248439465858, 1),
+            (4513559094146117, 2),
+            (7890971548908067, 3),
+            (8157356344118483, 1),
+            (2615250744386899, 2),
+            (8690095851526254, 3),
+            (6375711915077050, 1),
+            (6913859173121360, 1),
+            (6442889055042768, 2),
+            (2321386104303845, 0),
+            (2326509471271448, 2),
+            (5251583379644322, 2),
+            (1748270476758276, 3),
+            (4895722652190306, 1),
+            (3041631117224635, 3),
+            (1841236454324589, 3),
+            (2659862637316867, 2)
+        ]
+
+        ls_attempts_str = [(str(x), y) for (x, y) in ls_attempts]
+        problem = Problem185(ls_attempts_str)
+        self.assertEqual(4640261571849533, problem.solve())
 
 
-def main2():
-    ls_attempts = [
-        (5616185650518293, 2),
-        (3847439647293047, 1),
-        (5855462940810587, 3),
-        (9742855507068353, 3),
-        (4296849643607543, 3),
-        (3174248439465858, 1),
-        (4513559094146117, 2),
-        (7890971548908067, 3),
-        (8157356344118483, 1),
-        (2615250744386899, 2),
-        (8690095851526254, 3),
-        (6375711915077050, 1),
-        (6913859173121360, 1),
-        (6442889055042768, 2),
-        (2321386104303845, 0),
-        (2326509471271448, 2),
-        (5251583379644322, 2),
-        (1748270476758276, 3),
-        (4895722652190306, 1),
-        (3041631117224635, 3),
-        (1841236454324589, 3),
-        (2659862637316867, 2)
-    ]
-
-    ls_attempts = [(str(x), y) for (x, y) in ls_attempts]
-    a = Problem185(ls_attempts)
-    sol = a.solve()
-    print(sol)
-
-
-if __name__ == "__main__":
-    # main1()
-    main2()
+if __name__ == '__main__':
+    unittest.main()
