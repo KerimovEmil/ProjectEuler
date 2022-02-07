206aa81747ce5a59f72de32b61bf8f1c8ebcf7dc
KerimovEmil <emilkerimov93@gmail.com>
2022-02-06 19:53:12 -0500

    refactor test build to use list of bad or slow solutions only (#86)
    Co-authored-by: EmilKerimov <emil.kerimov93@gmail.com>

--- all_build_tests/test_build.py
+++ all_build_tests/test_build.py
@@ -1,18 +1,28 @@
 import unittest
 from importlib import import_module
+from os import listdir, path
 
-solved_quick_problems = [
-    1, 2, 3, 4, 5, 6, 10, 16, 18, 26, 30, 32, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 45, 47, 48, 49, 50, 51, 53, 54,
-    55, 56, 57, 59, 60, 61, 62, 63, 65, 66, 67, 69, 72, 73, 76, 81, 82, 89, 90, 92, 97, 99, 101, 108, 110, 111, 116,
-    120, 124, 125, 137, 139, 140, 144, 145, 152, 187, 193, 197, 204, 229, 233, 235, 236, 243, 356, 357, 486, 610, 668,
-    700, 739, 753]
-
-# todo make this the list to maintain eventually
 bad_or_slow_solutions = [46, 96, 179, 185, 211, 266, 330, 345, 401, 419, 420, 432]
 
-for prob in solved_quick_problems:
-    _mod = import_module(f'solutions.PE{prob}')
-    globals()[f'Solution{prob}'] = getattr(_mod, f'Solution{prob}')
+file_path = path.join(path.dirname(__file__), '..', 'solutions',)
+
+for file in listdir(file_path):
+    # ignore known files that don't follow convention
+    if file in ['new_problem.py', '__init__.py', '__pycache__',
+                'PE345_Hungarian_Algorithm.py', 'PE345_recursive.py']:
+        # todo combine PE345 into one file
+        continue
+
+    # get project euler problem number from file name
+    problem_number = int(file.split('.')[0][2:])
+
+    # don't run slow o bad solutions
+    if problem_number in bad_or_slow_solutions:
+        continue
+
+    # else import the solution class
+    _mod = import_module(f'solutions.PE{problem_number}')
+    globals()[f'Solution{problem_number}'] = getattr(_mod, f'Solution{problem_number}')
 
 
 if __name__ == '__main__':
