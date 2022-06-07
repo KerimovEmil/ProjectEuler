3c7b9d7eb8f9fdf5ceb8c6f5e1c310abff174b62
KerimovEmil <emilkerimov93@gmail.com>
2022-06-06 23:52:19 -0400

    cleaning up variable names

--- solutions/PE365.py
+++ solutions/PE365.py
@@ -39,8 +39,8 @@ class Problem365:
 
         self.ls_p = list(primes(min_prime, max_prime))
 
-    def get_combin_mod_p(self, p):
-        """Return (self.n choose self.k) mod p"""
+    def get_combination_mod_p(self, p):
+        """Return (self.n choose self.k) mod p where p is a prime"""
         n_tuple = number_base_rep(self.n, p)
         k_tuple = number_base_rep(self.k, p)
 
@@ -52,27 +52,27 @@ class Problem365:
     @timeit
     def solve(self):
         # get mod p for each p in self.ls_p
-        ls_p_mod_p = [(p, self.get_combin_mod_p(p)) for p in self.ls_p]
+        ls_p_mod_p = [(p, self.get_combination_mod_p(p)) for p in self.ls_p]
         total_len = len(ls_p_mod_p)
 
         ans = 0
 
-        # for p_i in range(total_len):
-        #     p, mod_p = ls_p_mod_p[p_i]
-        #     print(p)
-        #     for q_i in range(p_i + 1, total_len):
-        #         q, mod_q = ls_p_mod_p[q_i]
-        #         for r_i in range(q_i + 1, total_len):
-        #             r, mod_r = ls_p_mod_p[r_i]
-        #             ans += simple_crt_all_primes([mod_p, mod_q, mod_r], [p, q, r])
+        for p_i in range(total_len):
+            p, mod_p = ls_p_mod_p[p_i]
+            # print(p)
+            for q_i in range(p_i + 1, total_len):
+                q, mod_q = ls_p_mod_p[q_i]
+                for r_i in range(q_i + 1, total_len):
+                    r, mod_r = ls_p_mod_p[r_i]
+                    ans += simple_crt_all_primes([mod_p, mod_q, mod_r], [p, q, r])
 
-        for p, mod_p in ls_p_mod_p:
-            print(p)
-            for q, mod_q in ls_p_mod_p:
-                if p < q:
-                    for r, mod_r in ls_p_mod_p:
-                        if q < r:
-                            ans += simple_crt_all_primes([mod_p, mod_q, mod_r], [p, q, r])
+        # for p, mod_p in ls_p_mod_p:
+        #     print(p)
+        #     for q, mod_q in ls_p_mod_p:
+        #         if p < q:
+        #             for r, mod_r in ls_p_mod_p:
+        #                 if q < r:
+        #                     ans += simple_crt_all_primes([mod_p, mod_q, mod_r], [p, q, r])
         return ans
 
 
