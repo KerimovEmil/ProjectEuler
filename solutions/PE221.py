67b87666f95489a23833598518f30ce39f7f7760
KerimovEmil <emilkerimov93@gmail.com>
2025-02-14 22:35:06 -0500

    added some sample code

--- solutions/PE221.py
+++ solutions/PE221.py
@@ -1,7 +1,7 @@
 """
 PROBLEM
 
-We shall call a positive integer A an "Alexandrian integer", if there exist integers a,b,c such that:
+We shall call a positive integer "A" an "Alexandrian integer", if there exist integers a,b,c such that:
 
 A = a*b*c
 and
@@ -18,16 +18,19 @@ Solve time:
 from util.utils import timeit, is_int
 import unittest
 
-
 # a*b + b*c + a*c = 1
 # (a+b+c)^2 = a^2 + b^2 + c^2 + 2
+# (a+b+c)^3 = a^3 + b^3 + c^3 + 3 (a^2 b + a^2 c + a b^2 + a c^2 + b^2 c + b c^2) + 6 a b c
+# (a+b+c)^3 = a^3 + b^3 + c^3 + 3 (a*(ab + ac) + b*(ab + bc) + c*(ac + bc)) + 6 a b c
+# (a+b+c)^3 = a^3 + b^3 + c^3 + 3 (a*(ab + ac + bc) - abc + b*(ab + bc + ac) - abc + c*(ac + bc + ab) - abc) + 6 a b c
+# (a+b+c)^3 = a^3 + b^3 + c^3 + 3 (a + b + c - 3*abc) + 6abc
+# (a+b+c)^3 = a^3 + b^3 + c^3 + 3*(a + b + c) - 3abc
+# 3abc = a^3 + b^3 + c^3 + 3*(a+b+c) - (a+b+c)^3
 
-# if a = b then
-# a^2 + 2*a*c = 1
-# c = (1-a^2) / (2a)
-# (2a+c)^2 = 2*a^2 + c^2 + 2
-
-# wlog let a >= b >= c
+# wlog let |a| >= |b| >= |c|
+# fix k, k*b + b*c + k*c = 1
+# k*(b+c) = 1 - b*c
+# k = (1 - b*c) / (b+c)
 
 
 class Problem221:
@@ -36,7 +39,33 @@ class Problem221:
 
     @timeit
     def solve(self):
-        raise NotImplementedError('Please implement this method!')
+        ls_sol = []
+
+        def check(x, y, z):
+            if y == -z:
+                return False
+            return x == (1 - y*z) / (y+z)
+
+        def add(x, y, z):
+            p = x*y*z
+            print(f'{p=}, {x=}, {y=}, {z=}, {len(ls_sol)=}')
+            ls_sol.append(p)
+
+        a = 1
+        while len(ls_sol) < 2*self.n:
+            a += 1
+            for b in range(1, a + 1):
+                for c in range(1, b + 1):
+                    a_neg, b_neg, c_neg = -a, -b, -c
+                    if check(a_neg, b, c_neg):
+                        add(a_neg, b, c_neg)
+                    elif check(a_neg, b_neg, c):
+                        add(a_neg, b_neg, c)
+                    elif check(a, b_neg, c_neg):
+                        add(a, b_neg, c_neg)
+        ls_sol.sort()
+        print(ls_sol)
+        return ls_sol[self.n - 1]
 
 
 class Solution221(unittest.TestCase):
