9c912de2363956c2281bd669ea67dc23c6a1199b
KerimovEmil <emilkerimov93@gmail.com>
2026-09-20 23:36:34 -0400

    Speed up PE0266 via meet-in-the-middle subset sum, remove 266 from slow solutions list (#161)

--- all_build_tests/test_build.py
+++ all_build_tests/test_build.py
@@ -2,7 +2,8 @@ import unittest
 from importlib import import_module
 from os import listdir, path
 
-bad_or_slow_solutions = [185, 221, 266, 330, 365, 401, 420, 678]
+
+bad_or_slow_solutions = [185, 221, 330, 365, 401, 420, 678]
 
 file_path = path.join(path.dirname(__file__), '..', 'solutions',)
 
