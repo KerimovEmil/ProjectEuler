18308fe1c501edf451ead4c2b52f3b04264c37ca
KerimovEmil <emilkerimov93@gmail.com>
2026-09-21 22:24:32 -0400

    Apply suggestion from @KerimovEmil

--- solutions/PE0185.py
+++ solutions/PE0185.py
@@ -18,7 +18,32 @@ For instance, given the following guesses for a 5-digit secret sequence,
 
 The correct sequence 39542 is unique.
 
-Find the unique 16-digit secret sequence based on the 22 given guesses.
+Based on the following guesses,
+
+5616185650518293 ;2 correct
+3847439647293047 ;1 correct
+5855462940810587 ;3 correct
+9742855507068353 ;3 correct
+4296849643607543 ;3 correct
+3174248439465858 ;1 correct
+4513559094146117 ;2 correct
+7890971548908067 ;3 correct
+8157356344118483 ;1 correct
+2615250744386899 ;2 correct
+8690095851526254 ;3 correct
+6375711915077050 ;1 correct
+6913859173121360 ;1 correct
+6442889055042768 ;2 correct
+2321386104303845 ;0 correct
+2326509471271448 ;2 correct
+5251583379644322 ;2 correct
+1748270476758276 ;3 correct
+4895722652190306 ;1 correct
+3041631117224635 ;3 correct
+1841236454324589 ;3 correct
+2659862637316867 ;2 correct
+
+Find the unique 16-digit secret sequence.
 
 ANSWER: 4640261571849533
 Solve time: ~0.26 seconds
